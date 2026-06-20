126a2905cddff82c01292ac3e6cdab7f3ede9e93
felowctl <minidinik@gmail.com>
2026-06-21 01:35:10 +0300

    add xray proxy feature

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-
 REQUIRED_PACKAGES="hostapd dnsmasq iptables iptables-persistent"
 MISSING_PACKAGES=""
 
@@ -25,8 +24,8 @@ if [ -n "$MISSING_PACKAGES" ]; then
   exit 1
 fi
 
-systemctl unmask hostapd
-systemctl stop hostapd dnsmasq
+systemctl unmask hostapd > /dev/null
+systemctl stop hostapd dnsmasq > /dev/null
 
 echo Configuring hostapd
 
@@ -34,7 +33,7 @@ tee /etc/NetworkManager/conf.d/unmanaged.conf > /dev/null <<EOF
 [keyfile]
 unmanaged-devices=interface-name:wlan1
 EOF
-systemctl restart NetworkManager
+systemctl restart NetworkManager > /dev/null
 
 tee /etc/hostapd/hostapd.conf > /dev/null <<EOF
 interface=wlan1
@@ -85,16 +84,129 @@ tee /etc/sysctl.d/99-tproxy.conf > /dev/null <<EOF
 net.ipv4.ip_forward=1
 EOF
 
-iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+# iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE > /dev/null
+# iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT > /dev/null
+# iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT > /dev/null
+# 
+# netfilter-persistent save > /dev/null
+
+echo Installing xray
+
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+
+read -p "Enter trojan address" TROJAN_ADDRESS
+read -p "Enter trojan port" TROJAN_PORT
+read -p "Enter trojan password" TROJAN_PASSWORD
+read -p "Enter trojan sni" TROJAN_SNI
+read -p "Enter trojan fingerprint" TROJAN_FINGERPRINT
+
+sudo tee /usr/local/etc/xray/config.json > /dev/null <<EOF
+{
+  "log": {
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "tag": "tproxy-in",
+      "port": 12345,
+      "protocol": "tunnel",
+      "settings": {
+        "network": "tcp,udp",
+        "followRedirect": true
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": ["http", "tls"]
+      },
+      "streamSettings": {
+        "sockopt": {
+          "tproxy": "tproxy"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "tag": "proxy",
+      "protocol": "trojan",
+      "settings": {
+        "servers": [
+          {
+            "address": "${TROJAN_ADDRESS}",
+            "port": ${TROJAN_PORT},
+            "password": "${TROJAN_PASSWORD}"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "tls",
+        "tlsSettings": {
+          "serverName": "${TROJAN_SNI}",
+          "fingerprint": "${TROJAN_FINGERPRINT}"
+        }
+      }
+    },
+    {
+      "tag": "direct",
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ],
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [
+      {
+        "type": "field",
+        "inboundTag": ["tproxy-in"],
+        "outboundTag": "proxy"
+      }
+    ]
+  }
+}
+EOF
+
+
+tee "/etc/systemd/system/xray-routing.service" > /dev/null <<EOF
+[Unit]
+Description=Xray TProxy policy routing (pi-proxy-bridge)
+After=network.target
+ 
+[Service]
+Type=oneshot
+ExecStart=/sbin/ip route add local default dev lo table 100
+ExecStart=/sbin/ip rule add fwmark 1 table 100
+RemainAfterExit=yes
+ 
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+echo Setting up iptables rules
+
+iptables -t mangle -N XRAY
+iptables -t mangle -A XRAY -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A XRAY -d 100.64.0.0/10 -j RETURN
+iptables -t mangle -A XRAY -d 127.0.0.0/8 -j RETURN
+iptables -t mangle -A XRAY -d 169.254.0.0/16 -j RETURN
+iptables -t mangle -A XRAY -d 172.16.0.0/12 -j RETURN
+iptables -t mangle -A XRAY -d 192.0.0.0/24 -j RETURN
+iptables -t mangle -A XRAY -d 224.0.0.0/4 -j RETURN
+iptables -t mangle -A XRAY -d 240.0.0.0/4 -j RETURN
+iptables -t mangle -A XRAY -d 255.255.255.255/32 -j RETURN
+iptables -t mangle -A XRAY -d 192.168.0.0/16 -j RETURN
+iptables -t mangle -A XRAY -p tcp -j TPROXY --on-port 12345 --tproxy-mark 1
+iptables -t mangle -A XRAY -p udp -j TPROXY --on-port 12345 --tproxy-mark 1
+iptables -t mangle -A PREROUTING -j XRAY
 
 netfilter-persistent save > /dev/null
+
 
 echo Starting services
 
 sysctl --system > /dev/null
 
-systemctl daemon-reload
-systemctl enable hostapd dnsmasq wlan1-static-ip
-systemctl start hostapd dnsmasq wlan1-static-ip 
+systemctl daemon-reload > /dev/null
+systemctl enable hostapd dnsmasq xray wlan1-static-ip xray-routing > /dev/null
+systemctl start hostapd dnsmasq xray wlan1-static-ip xray-routing > /dev/null
